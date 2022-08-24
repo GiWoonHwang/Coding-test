66cd05553bea9c9a8e02b630e5baa2c7bd43c2e2
JangDeukChun <85157729+JangDeukchun@users.noreply.github.com>
2022-08-24 09:50:22 +0900

    Update 2.py

--- Level2/2.py
+++ Level2/2.py
@@ -18,11 +18,12 @@ s	                             return
 '''
 
 
+
 def cal(s):
-    s = s.split(" ")
-    for i in range(len(s)):
-        s[i] = s[i][:1].upper() + s[i][1:].lower()
-    return ' '.join(s)
+    s = s.split(" ") # 공백을 기준으로 나누어준 후
+    for i in range(len(s)): 
+        s[i] = s[i][:1].upper() + s[i][1:].lower() # 처리
+    return ' '.join(s) # 다시 공백을 기준으로 합쳐준다.
 
 
 
