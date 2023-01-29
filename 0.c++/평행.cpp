45ba46716d9816c612590effdf1e5262ce7ee6be
GiWoon <rlaejrqo465@naver.com>
2023-01-29 12:32:57 +0900

    d -> dots

--- 0.c++/평행.cpp
+++ 0.c++/평행.cpp
@@ -33,9 +33,9 @@ using namespace std;
 
 int solution(vector<vector<int>> dots){
     if(
-        double(double(dots[3][1]-dots[0][1])/double(dots[3][0]-dots[0][0])) == double(double(d[2][1]-d[1][1])/double(d[2][0]-d[1][0]))
-        || double(double(d[2][1]-d[0][1])/double(d[2][0]-d[0][0]))          == double(double(d[3][1]-d[1][1])/double(d[3][0]-d[1][0]))
-        || double(double(d[1][1]-d[0][1])/double(d[1][0]-d[0][0]))          == double(double(d[2][1]-d[3][1])/double(d[2][0]-d[3][0]))) return 1;
+        double(double(dots[3][1]-dots[0][1])/double(dots[3][0]-dots[0][0])) == double(double(dots[2][1]-dots[1][1])/double(dots[2][0]-dots[1][0]))
+        || double(double(dots[2][1]-dots[0][1])/double(dots[2][0]-dots[0][0]))          == double(double(dots[3][1]-dots[1][1])/double(dots[3][0]-dots[1][0]))
+        || double(double(dots[1][1]-dots[0][1])/double(dots[1][0]-dots[0][0]))          == double(double(dots[2][1]-dots[3][1])/double(dots[2][0]-dots[3][0]))) return 1;
     
     return 0;
 
