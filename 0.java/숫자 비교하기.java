f3364067c2e9ee2c230776ccfa339a02b229c2fd
JangDeukchun <rlaejrqo465@naver.com>
2023-04-17 10:23:24 +0900

    제한사항 생각하기

--- 0.java/숫자 비교하기.java
+++ 0.java/숫자 비교하기.java
@@ -3,7 +3,7 @@
  */
 
  class Solution {
-    private int solution(int num1, int num2) {
+    public int solution(int num1, int num2) {
         int answer = (num1 == num2) ? 1 : -1;
         return answer;
     }
