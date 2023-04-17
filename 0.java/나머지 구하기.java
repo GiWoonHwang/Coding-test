20464aea678558eccba9a57f905e84c979968a23
JangDeukchun <rlaejrqo465@naver.com>
2023-04-17 11:32:21 +0900

    할말없음

---
+++ 0.java/나머지 구하기.java
@@ -0,0 +1,14 @@
+/*
+ * num1과 num2 나눈 나머지를 return 하라
+ */
+
+ class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        boolean val = (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100);
+        if(val){
+        answer = num1 % num2;
+        }
+        return answer;
+    }
+}
