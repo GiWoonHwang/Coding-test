93ad7d19a1aae8a89c05e2c250f8f4801e839314
JangDeukchun <rlaejrqo465@naver.com>
2023-04-17 10:11:49 +0900

    자바를 하지 않으면 대화가 되지 않는다

---
+++ 0.java/숫자 비교하기.java
@@ -0,0 +1,10 @@
+/*
+ * 매개변수가 같으면 1, 다르면 -1을 리턴하라
+ */
+
+ class Solution {
+    private int solution(int num1, int num2) {
+        int answer = (num1 == num2) ? 1 : -1;
+        return answer;
+    }
+}
