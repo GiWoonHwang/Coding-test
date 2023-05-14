5124bb8b23b01cdafef354ba7de3e95144f163ba
JangDeukchun <rlaejrqo465@naver.com>
2023-05-14 15:12:41 +0900

    Arrays.stram(array).filter(v->v==n).count();

---
+++ 0.java/조건에 맞게 수열 변환하기3.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        if(k%2 == 0){
+            return IntStream.of(arr).map(i->i+k).toArray();
+        }
+        return IntStream.of(arr).map(i->i*k).toArray();
+    }
+}
