cd8a67d1da4dade06d8b84c8fbfd298782257243
JangDeukchun <rlaejrqo465@naver.com>
2023-06-05 12:13:29 +0900

    stream

---
+++ 0.java/조건에 맞게 수열 변환하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        return Arrays.stream(arr).map(v -> v >=50 && v % 2 == 0 ?  v / 2 : v < 50 && v % 2 ==  1 ? v * 2 : v ).toArray();
+
+    }
+}
