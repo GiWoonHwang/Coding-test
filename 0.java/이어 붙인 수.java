afb6820132126e7b34646dc168e6ac8bdf3da0fe
JangDeukchun <rlaejrqo465@naver.com>
2023-06-07 16:18:09 +0900

    숫자를 문자로 변환할 때 를 붙여준다

---
+++ 0.java/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String jjaksu = "";
+        String holsu = "";
+
+        for(int num : num_list) {
+            if(num % 2 == 0) {
+                jjaksu += num + "";
+            } else {
+                holsu += num + "";
+            }
+        }
+
+        return Integer.parseInt(jjaksu) + Integer.parseInt(holsu);
+    }
+}
