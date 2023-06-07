36f13af55dde9ea5cb685a86ea01de93cb4d6f9c
JangDeukchun <rlaejrqo465@naver.com>
2023-06-07 15:27:28 +0900

    i%n == 0이 핵심이다

---
+++ 0.java/n개 간격의 원소들.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<num_list.length; i++) {
+            if(i % n == 0) list.add(num_list[i]);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
