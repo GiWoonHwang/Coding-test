131a6b963407ba43618e2e117ee39f4e179c3f35
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2024-04-08 09:51:40 +0900

    update

--- cote/press.prc.py
+++ cote/press.prc.py
@@ -1,4 +1,13 @@
-s = 'abcabcabcabc6de'
+s = 'abc'
+
+'''
+1. 문자열을 1 ~ len(s)만큼 쪼갠다
+2. 쪼갠문자열을 temp 변수에 담은 후  다음 문자열을 비교한다
+3. 만약 비고해서 같다면 카운트를 올려준다.
+4. 다르다면 쪼갠 문자열을 b라는 변수에 갱신한다.
+    4-1 다르지만 중복된 적이 있다면 b라는 변수에 카운트를 추가해서 갱신한다.
+5. temp를 직전에 비교했던 다음 문자열로 갱신한다. (계속해서 다음 문자열과 비교하기 위함)
+'''
 
 def solution(s):
     result = []
@@ -9,16 +18,22 @@ def solution(s):
 
         temp = s[:i] # i만큼 글자를 자름 처음엔 1
 
-        for j in range(i, len(s)+i):
+        for j in range(i, len(s)+i, i):
             # temp의 글자와 temp 바로 뒤의 글자가 같다면
+            # 글자가 같으면 갱신해 줄 필요가 없음, 나중에 중복된 숫자만 붙여주면 됨
             if temp == s[j:j+i]:
                 cnt += 1
+            # 중복되지 않는다.
             else:
+                # 지금까지 중복되는게 하나라도 있다면
                 if cnt != 1:
+                    # 문자열을 생성할때 중복되는 숫자를 추가해 준다. 
                     b = b + str(cnt) + temp
                 else:
+                    # 
                     b = b + temp
                 
+                # temp를 갱신한다.
                 temp = s[j:j+i]
                 cnt = 1
             
@@ -26,3 +41,4 @@ def solution(s):
     return min(result)
             
     
+
--- cote/press.py
+++ cote/press.py
@@ -1,29 +1,15 @@
-# 글자를 나누어 앞 단어와 비교 (1부터 len(s)까지 계속 잘라본다)
-
 def solution(s):
     result = []
     for i in range(1, len(s)+1):
         b = ''
         cnt = 1
-        '''
-        temp a
-        temp aa
-        temp aab
-        temp aabb
-        temp aabba
-        temp aabbac
-        temp aabbacc
-        temp aabbaccc
-        '''
-        temp = s[:i] # i만큼 글자를 잘랐다.
+  
+        temp = s[:i] 
         
         for j in range(i, len(s)+i, i):
-            # 앞뒤 글자가 같다.
             if temp == s[j:i+j]:
                 cnt += 1
-            # 앞뒤 글자가 같지 않다.
             else:
-                # 더 이상 중복되지 않는다.
                 if cnt != 1:
                     b = b + str(cnt)+ temp
                 else:
