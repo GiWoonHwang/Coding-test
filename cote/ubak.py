8af3e63f350eba28f049fb2696c459d50e48eda6
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2024-04-12 09:02:54 +0900

    ubak update

--- cote/ubak.py
+++ cote/ubak.py
@@ -1,6 +1,3 @@
-# 꼭지점 마다 선을 그어 사다리꼴을 만든다
-# 사다리꼴의 넓이를 구해 전부 더한다 
-
 def collatz(k):
     result = []
     while k != 1:
@@ -19,15 +16,15 @@ def solution(k, ranges):
     
     for i in ranges:
         total = 0
-        ubakRange = ubak[i[0]: len(ubak)+i[1]]
+        ubak_range = ubak[i[0]: len(ubak)+i[1]]
 
                          
         if i[0] >= i[1] + len(ubak):
             answer.append(-1)
             continue
 
-        for i in range(len(ubakRange)-1):
-            total += (((ubakRange[i] + ubakRange[i+1]) * 1) / 2)
+        for i in range(len(ubak_range)-1):
+            total += (((ubak_range[i] + ubak_range[i+1]) * 1) / 2)
 
         answer.append(total)
     
