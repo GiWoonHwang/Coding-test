17a33b6f362a3b7e7e82d9adcbee3ff153966342
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2024-05-30 16:04:19 +0900

    교점에 별 찍기

--- cote/cross.line.prc.py
+++ cote/cross.line.prc.py
@@ -1,12 +1,7 @@
-'''
-각 직선마다 교점을 직고
-교점을 포함하는 최소 사각형을 리턴하라
-'''
-
-
 line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]
 
 def solution(line):
+    
     stars = []
 
     # 교점 구하기
@@ -26,47 +21,45 @@ def solution(line):
     # 중복 제거
     stars = list(set(stars))
 
-    # y가 크면 위에서부터 아래로 그릴 수있다.
-    # x가 크먄 작은 순서대로 그릴 수 있다
-    stars.sort(key =lambda x:(x[1], -x[0]), reverse=True)
 
-    # x,y의 최소 범위를 구해야 최소한의 사각형을 그릴 수 있다
+    # y를 내림차순 정렬하여 위에서부터 아래로 그런다
+    # x를 오름차순 하여 왼쪽부터 오른쪽으로 그린다
+    stars.sort(key= lambda x: (x[1], -x[0]), reverse=True)
+
+
+    # 교점의 범위를 구해서 최소한의 사각형을 그린다.
     x_list = []
     y_list = []
 
     for i in stars:
         x_list.append(i[0])
         y_list.append(i[1])
-    x_max = max(x_list) 
+
+    # 각 좌표에 해당하는 최소값을 구해 범위를 잡는다.
+    x_max = max(x_list)
     x_min = min(x_list)
+
     y_max = max(y_list)
     y_min = min(y_list)
 
 
-    new_coordinates = [ ['.'] for i in range(x_min,x_max+1) for _ in range(y_min,y_max+1)]
+    # 새로운 좌표를 통해 새로운 사각형을 만든다.
+    new_sagak = [['.'] for i in range(x_min, x_max+1) for _ in range(y_min,y_max)]
     for i in stars:
-        x = i[0] - x_min
+        x = stars[0] - x_min
         y = y_max - i[1]
-        new_coordinates[y][x] = '*'
+        new_sagak[y][x]  = '*'
 
     result = []
 
-    for i in new_coordinates:
+    for i in new_sagak:
         result.append("".join(i))
+
+
+
+
+
     return result
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
